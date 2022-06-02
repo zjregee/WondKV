3309a11a9ab9f56e4b2938917f81afd9fd804146
zjregee <zjregee@gmail.com>
2022-06-02 18:08:45 +0800

    feature: update

--- src/storage/db_file.rs
+++ src/storage/db_file.rs
@@ -1,4 +1,8 @@
-use std::fs::File;
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions, self};
+use std::io::Write;
+use std::os::unix::prelude::FileExt;
+use std::path::Path;
 use crate::utils::crc32;
 use crate::storage::entry;
 
@@ -6,12 +10,23 @@ pub struct DBFile {
     id: u32,
     path: String,
     offset: u32,
-    file: File
+    file: Option<File>
 }
 
 impl DBFile {
     pub fn new(path: String, file_id: u32) -> Option<DBFile> {
-        None
+        let file_path = Path::new(&path).join(format!("{}.data.hash", file_id));
+        let ret = OpenOptions::new().read(true).write(true).open(file_path);
+        if ret.is_err() {
+            return None;
+        }
+        let f = ret.ok().unwrap();
+        Some(DBFile {
+            id: file_id,
+            offset: f.metadata().unwrap().len() as u32,
+            file: Some(f),
+            path,
+        })
     }
 
     pub fn read(&self, mut offset: u32) -> Option<entry::Entry> {
@@ -49,11 +64,21 @@ impl DBFile {
     }
 
     pub fn read_buf(&self, offset: u32, len: u32) -> Option<Vec<u8>> {
-
-        None
+        if self.file.is_none() {
+            return None;
+        }
+        let mut buf = Vec::with_capacity(len as usize);
+        let ret = self.file.as_ref().unwrap().read_at(&mut buf, offset as u64);
+        if ret.is_err() {
+            return None;
+        }
+        Some(buf)
     }
 
     pub fn write(&mut self, entry: entry::Entry) -> bool {
+        if self.file.is_none() {
+            return false;
+        }
         if !entry.valid {
             return false;
         }
@@ -62,22 +87,61 @@ impl DBFile {
             return false;
         }
         let buf = buf.unwrap();
-
+        let ret = self.file.as_ref().unwrap().write_all(&buf);
+        if ret.is_err() {
+            return false;
+        }
         self.offset += entry.size();
         true
     }
 
-    pub fn close(&mut self) -> bool {
-
-        false
+    pub fn close(&mut self, sync: bool) -> bool {
+        if self.file.is_none() {
+            return false;
+        }
+        let mut ret = true;
+        if sync {
+            ret = self.sync();
+        }
+        self.file = None;
+        ret
     }
 
-    pub fn sync() -> bool {
-
-        false
+    pub fn sync(&mut self) -> bool {
+        if self.file.is_none() {
+            return false;
+        }
+        let ret = self.file.as_ref().unwrap().sync_all();
+        ret.is_ok()
     }
 }
 
-pub fn build(path: String) {
-    
+pub fn build(path: String) -> Option<(HashMap<u32, DBFile>, u32)> {
+    let dir = fs::read_dir(path.clone());
+    if dir.is_err() {
+        return None;
+    }
+    let dir = dir.ok().unwrap();
+    let mut all_id = vec![];
+    for entry in dir {
+        let file_name = entry.ok().unwrap().file_name().to_str().unwrap().to_string();
+        if file_name.contains(".data.hash") {
+            let id = file_name.split(".").next().unwrap().parse::<u32>().ok().unwrap();
+            all_id.push(id);
+        }
+    }
+    let mut active_id = 0;
+    let mut arch_files = HashMap::new();
+    if all_id.len() > 0 {
+        all_id.sort();
+        active_id = *all_id.last().unwrap();
+        for id in all_id {
+            let file = DBFile::new(path.clone(), id);
+            if file.is_none() {
+                return None;
+            }
+            arch_files.insert(id, file.unwrap());
+        }
+    }
+    Some((arch_files, active_id))
 }
